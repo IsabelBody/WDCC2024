9d2c4eaeebf0b4b8559103a2c5c01f6bc8750e58
ls-ron <rony24th@gmail.com>
2024-08-03 14:27:39 +1200

    changed edge values and distances

--- backend/dijkstra_implementation.py
+++ backend/dijkstra_implementation.py
@@ -17,17 +17,16 @@ graph.add_node("g_7", "Lorem ipsum")
 graph.add_node("g_8", "Lorem ipsum")
 
 # the various edges connecting the nodes
-graph.add_edge("g_1", "g_2", 110)
-graph.add_edge("g_1", "g_3", 125)
-graph.add_edge("g_1", "g_4", 108)
-graph.add_edge("g_1", "g_4", 108)
-graph.add_edge("g_1", "g_5", 108)
-graph.add_edge("g_2", "g_3", 108)
-graph.add_edge("g_4", "g_5", 108)
-graph.add_edge("g_4", "g_6", 108)
-graph.add_edge("g_5", "g_6", 108)
-graph.add_edge("g_6", "g_7", 108)
-graph.add_edge("g_7", "g_8", 108)
+graph.add_edge("g_1", "g_2", 3)
+graph.add_edge("g_1", "g_3", 5)
+graph.add_edge("g_1", "g_4", 7)
+graph.add_edge("g_1", "g_5", 3)
+graph.add_edge("g_2", "g_3", 6)
+graph.add_edge("g_4", "g_5", 10)
+graph.add_edge("g_4", "g_6", 4)
+graph.add_edge("g_5", "g_6", 5)
+graph.add_edge("g_6", "g_7", 5)
+graph.add_edge("g_7", "g_8", 1)
 
 
 # idk what this does yet
