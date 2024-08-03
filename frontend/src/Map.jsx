34d5276388a74344c7a32c8e3b95ca23b42e731b
harbassan <harbassan@hotmail.com>
2024-08-03 18:35:52 +1200

    update frontend

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,41 +1,46 @@
+import { useState, useEffect } from 'react';
 import Console from './Console';
 import background from './assets/Untitled.png';
-import { useState } from 'react';
+import axios from 'axios';
 
 const MapPage = ({ cb }) => {
-
-
+    const [path, setPath] = useState([])
+    
     const ClickHandler = ({name, hoverText}) => {
-
         const [isHover, setIsHover] = useState(false)
-        
         return (
             <>
-            <button className={`galaxyButton ${name}`} onMouseLeave={() => setIsHover(false)} onMouseEnter={() => setIsHover(true)} onClick={() => cb(`/info/${name}`)}></button>
-            {isHover && (<div>{hoverText}</div>)}
+                <button className={`galaxyButton ${name}`} onMouseLeave={() => setIsHover(false)} onMouseEnter={() => setIsHover(true)} onClick={() => cb(`/info/${name}`)}></button>
+                {isHover && (<div>{hoverText}</div>)}
             </>
         )
     }
+
+    useEffect(() => {
+        const f = async () => {
+            await axios.post("http://localhost:5000/select-galaxy", { node: 5 });
+            const res = await axios.get("http://localhost:5000/shortest-path");
+            console.log(res.data.path)
+        }
+        f();
+    }, [path])
+
     return (
-    <>
-        <img className="background" src={background}/>
-        <button onClick={() => cb("/info/WDCCxSESA")}>WDCCxSESA</button>
-        <ClickHandler name={"WDCCxSESA"} hoverText="WDCCxSESA Galaxy"/>
-        <ClickHandler name={"Andromeda"} hoverText="Andromeda Galaxy"/>
-        <ClickHandler name={"Draco"} hoverText="Draco Galaxy"/>
-        <ClickHandler name={"Phoenix"} hoverText="Phoenix Galaxy"/>
-        {/* <ClickHandler name={"Leo"} hoverText="Leo Galaxy"/>
-        <ClickHandler name={"Tukana Dwarf"} hoverText="Tukana Dwarf Galaxy"/>
-        <ClickHandler name={"Cosmos Redshift7"} hoverText="Cosmos Redshift7 Galaxy"/>
-        <ClickHandler name={"Aquarius dwarf"} hoverText="Aquarius dwarf Galaxy"/> */}
+        <>
+            <img className="background" src={background}/>
+            <button onClick={() => cb("/info/WDCCxSESA")}>WDCCxSESA</button>
+            <ClickHandler name={"WDCCxSESA"} hoverText="WDCCxSESA Galaxy"/>
+            <ClickHandler name={"Andromeda"} hoverText="Andromeda Galaxy"/>
+            <ClickHandler name={"Draco"} hoverText="Draco Galaxy"/>
+            <ClickHandler name={"Phoenix"} hoverText="Phoenix Galaxy"/>
+            {/* <ClickHandler name={"Leo"} hoverText="Leo Galaxy"/>
+            <ClickHandler name={"Tukana Dwarf"} hoverText="Tukana Dwarf Galaxy"/>
+            <ClickHandler name={"Cosmos Redshift7"} hoverText="Cosmos Redshift7 Galaxy"/>
+            <ClickHandler name={"Aquarius dwarf"} hoverText="Aquarius dwarf Galaxy"/> */}
 
-
-        <Console cb={cb}/>
-    </>
+            <Console cb={cb}/>
+        </>
     )
-    
 }
-
-
  
 export default MapPage;
