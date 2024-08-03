c119284f8dbbe6142bb4075d7b74e51d3e4e0169
harbassan <harbassan@hotmail.com>
2024-08-03 17:05:45 +1200

    update frontend

--- frontend/src/Info.jsx
+++ frontend/src/Info.jsx
@@ -1,8 +1,19 @@
+import andromeda from './assets/andromeda.png'
+import Console from './Console'
+
 const InfoPage = ({ galaxy, cb }) => {
+
+    const desc = "Andromeda is a spiral galaxy approximately 2.537 million light-years from Earth, and the nearest major galaxy to the Milky Way. Its name is derived from the area of the sky in which it appears, the constellation of Andromeda."
+
     return (
     <>
-        {galaxy}
+        <img className="background" src={andromeda}/>
+        <div className="panel">
+            <h1>{galaxy}</h1>
+            <p>{desc}</p>
+        </div>
         <button onClick={() => cb("/")}>Back</button>
+        <Console cb={cb}/>
     </>
     )
     
--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import Console from './Console';
 import background from './assets/Untitled.png';
 
 const MapPage = ({ cb }) => {
@@ -11,29 +11,6 @@ const MapPage = ({ cb }) => {
     </>
     )
     
-}
-
-const Console = ({ cb }) => {
-    const [input, setInput] = useState("")
-
-    const command = (input) => {
-        const args = input.split(" ")
-        switch (args[0]) {
-            case "info":
-                console.log(args[1])
-                cb("/info/" + args[1])
-                break
-            default:
-                console.log("Unknown command")
-        }
-    }
-
-    return (
-        <div className="console">
-            <input value={input} onChange={(e) => setInput(e.target.value)} />
-            <button onClick={() => command(input)}>Log</button>
-        </div>
-    )
 }
 
 export default MapPage;
